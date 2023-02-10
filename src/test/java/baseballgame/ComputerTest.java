17a48e8b1fe192e99aa9d300a6458bb9e71ac9ac
mingyu <mingyu.oh15b@gmail.com>
2023-02-10 15:32:15 +0900

    test: judgingStrikeOrBall() TC 추가

--- src/test/java/baseballgame/ComputerTest.java
+++ src/test/java/baseballgame/ComputerTest.java
@@ -9,7 +9,7 @@ class ComputerTest {
     Computer computer = new Computer();
 
     @Test
-    void answerGenerateTest() {
+    void answerGenerate() {
         int answer = computer.answerGenerate();
 
         assertThat(answer).isLessThan(1000);
@@ -25,7 +25,7 @@ class ComputerTest {
     }
 
     @Test
-    void isDifferPlaceValueTest() {
+    void isDifferPlaceValue() {
         assertThat(computer.isDifferPlaceValue(1,2,3)).isTrue();
         assertThat(computer.isDifferPlaceValue(1,1,1)).isFalse();
         assertThat(computer.isDifferPlaceValue(1,1,2)).isFalse();
@@ -34,11 +34,17 @@ class ComputerTest {
     }
 
     @Test
-    void comparePitchingToAnswerTest() {
+    void judgingStrikeOrBall() {
+        assertThat(computer.judgingStrikeOrBall('1', '2', '3', '1', '2', '3')).isEqualTo("3개의 숫자를 모두 맞히셨습니다!");
+        assertThat(computer.judgingStrikeOrBall('1','0','4','1','2','3')).isEqualTo("1스트라이크");
+        assertThat(computer.judgingStrikeOrBall('1','2','4','1','4','2')).isEqualTo("2볼 1스트라이크");
+        assertThat(computer.judgingStrikeOrBall('1','2','4','5','6','7')).isEqualTo("낫싱");
+    }
+    @Test
+    void comparePitchingToAnswer() {
         assertThat(computer.comparePitchingToAnswer(123,123)).isEqualTo("3개의 숫자를 모두 맞히셨습니다!");
         assertThat(computer.comparePitchingToAnswer(104,123)).isEqualTo("1스트라이크");
         assertThat(computer.comparePitchingToAnswer(124,142)).isEqualTo("2볼 1스트라이크");
 
     }
-
 }
