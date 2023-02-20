d4f757ca43a329e0747c25c772abe7e01bc8b421
mingyu <mingyu.oh15b@gmail.com>
2023-02-21 02:03:59 +0900

    fix(Referee): 필드값 초기화하는 메서드 추가

--- src/main/java/baseballgameRefactorV2/model/Referee.java
+++ src/main/java/baseballgameRefactorV2/model/Referee.java
@@ -2,9 +2,10 @@ package baseballgameRefactorV2.model;
 
 public class Referee {
     private static final int DIGIT_LENGTH = 3;
-    private int numberOfStrikes = 0;
-    private int numberOfBalls = 0;
+    private int numberOfStrikes;
+    private int numberOfBalls;
     public Result judge(Balls userBalls, Balls answerBalls) {
+        initBallStatusNumber();
         for(int currentDigit = 0; currentDigit < DIGIT_LENGTH ; currentDigit++) {
            countStrikeNumber(userBalls, answerBalls, currentDigit) ;
            countBallNumber(userBalls, answerBalls, currentDigit);
@@ -12,6 +13,12 @@ public class Referee {
 
         return new Result(numberOfStrikes, numberOfBalls);
     }
+
+    private void initBallStatusNumber() {
+        numberOfStrikes = 0;
+        numberOfBalls = 0;
+    }
+
     private void countStrikeNumber(Balls userBalls, Balls answerBalls, int digit) {
         if (userBalls.isStrike(answerBalls, digit)) {
             this.numberOfStrikes++;
