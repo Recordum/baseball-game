8cbbf4bc1a77b796708b7ce26ff8fbfff27f8af7
mingyu <mingyu.oh15b@gmail.com>
2023-02-21 02:03:16 +0900

    feat(GameStatus): 기능 추가

--- src/main/java/baseballgameRefactorV2/util/GameStatus.java
+++ src/main/java/baseballgameRefactorV2/util/GameStatus.java
@@ -3,5 +3,13 @@ package baseballgameRefactorV2.util;
 public enum GameStatus {
     FINISH,
     ON_GOING,
-    RESTART
+    RESTART;
+
+    public boolean isRestart(){
+        return this == RESTART;
+    }
+
+    public boolean isNotFinish(){
+        return this == ON_GOING || this == RESTART;
+    }
 }
