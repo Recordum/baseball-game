cb079c979b3a12fe10df7e9cbd7184ac2456a0db
mingyu <mingyu.oh15b@gmail.com>
2023-02-21 02:05:01 +0900

    feat(application): 기능 구현 완료

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -1,12 +1,18 @@
 import baseballgame.BaseballGame;
 import baseballgameRefactorV1.BaseBallGameRefactorV1;
+import baseballgameRefactorV2.controller.BaseBallGameController;
+import baseballgameRefactorV2.model.Referee;
 
 public class application {
     public static void main(String[] args) {
+        Referee referee = new Referee();
+        BaseBallGameController baseBallGameController = new BaseBallGameController(referee);
 //        BaseballGame baseballGame = new BaseballGame();
 //
 //        baseballGame.play();
 
-        BaseBallGameRefactorV1.play();
+//        BaseBallGameRefactorV1.play();
+        baseBallGameController.play();
+
     }
 }
