a863fb13a6108d04d9f7207ed8fb34fb2b1cc27a
mingyu <mingyu.oh15b@gmail.com>
2023-02-21 01:10:05 +0900

    feat(ResultView): 사용자에게 결과 view를 보여주는 기능 추가

---
+++ src/main/java/baseballgameRefactorV2/view/ResultView.java
@@ -0,0 +1,38 @@
+package baseballgameRefactorV2.view;
+
+import baseballgameRefactorV2.util.ConsoleMessage;
+import baseballgameRefactorV2.util.ExceptionMessage;
+import baseballgameRefactorV2.util.GameStatus;
+
+import java.util.Scanner;
+
+public class ResultView {
+
+    private static final int RESTART = 1;
+    private static final int END = 2;
+
+    public static GameStatus viewResolver(String view){
+        Scanner scanner = new Scanner(System.in);
+        int gameStatus = 0;
+        System.out.println(view);
+        if(view.equals(ConsoleMessage.CORRECT.printMessage())){
+
+            System.out.println(ConsoleMessage.RESULT_MESSAGE);
+            gameStatus = scanner.nextInt();
+
+            return restartOrFinish(gameStatus);
+        }
+        return GameStatus.ON_GOING;
+    }
+
+    private static GameStatus restartOrFinish(int gameStatus) {
+        if(gameStatus == RESTART){
+            return GameStatus.RESTART;
+        }
+        if(gameStatus == END){
+            return GameStatus.FINISH;
+        }
+
+        throw new IllegalStateException(ExceptionMessage.INVALID_RESULT_NUMBER.printMessage());
+    }
+}
