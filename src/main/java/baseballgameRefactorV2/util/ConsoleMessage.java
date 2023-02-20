e1595c372cbf6bb1696dbc98db47285255f831b3
mingyu <mingyu.oh15b@gmail.com>
2023-02-21 01:09:32 +0900

    feat(ConsoleMessage): ConsoleMessage RESULT, INPUT 추가

--- src/main/java/baseballgameRefactorV2/util/ConsoleMessage.java
+++ src/main/java/baseballgameRefactorV2/util/ConsoleMessage.java
@@ -5,7 +5,9 @@ public enum ConsoleMessage {
     STRIKE("스트라이크"),
     BALL("%d 볼"),
     STRIKE_BALL("%d 스트라이크, %d 볼"),
-    CORRECT("정답 입니다");
+    CORRECT("정답 입니다"),
+    INPUT_MESSAGE("숫자를 입력해주세요."),
+    RESULT_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
 
     private final String message;
     ConsoleMessage(String message) {
